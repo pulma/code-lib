e4ddb1f843d068e7b7be155148ee53b237ca08af
pulma <pulma@163.com>
2024-04-05 23:39:58 +0800

    add ScannerTest2

--- Base/src/pulma/base/ScannerTest.java
+++ Base/src/pulma/base/ScannerTest.java
@@ -5,7 +5,13 @@ import java.util.Scanner;
 public class ScannerTest {
     public static void main(String[] args) {
         Scanner sc =  new Scanner(System.in);
+
+        System.out.println("请输入一个数字： ");
         int num =  sc.nextInt();
         System.out.println("输入的数字是："+num);
+
+        System.out.println("请输入一个字符串： ");
+        String str = sc.next();
+        System.out.println("输入的文字是： "+str);
     }
 }
