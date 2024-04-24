b381a2e50462720f0e0f5e8718d85e4c5f01fe4f
pulma <pulma@163.com>
2024-04-24 22:26:34 +0800

    增加mathTest

---
+++ Base/src/pulma/tools/MathTest.java
@@ -0,0 +1,12 @@
+package pulma.tools;
+
+public class MathTest {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-3));   //绝对值
+        System.out.println(Math.ceil(2.1));   //绝对值
+        System.out.println(Math.floor(3.9));   //绝对值
+        System.out.println(Math.round(3.4));   //绝对值
+        System.out.println(Math.round(2.6));   //绝对值
+        System.out.println(Math.floor(Math.PI));   //绝对值
+    }
+}
